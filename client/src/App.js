505aad2a3f2ffba0bf70621107f7da538eb5b44f
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-06-18 16:00:12 -0700

    create profile finished need to addfunc

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 import './App.css';
 
@@ -36,6 +37,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>

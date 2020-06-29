04668869d14cc58a65cb6a0d8e498788c08e645b
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-06-29 15:02:31 -0700

    posts are being setup

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 import './App.css';
 
@@ -65,6 +66,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </section>
           </Switch>
         </Fragment>

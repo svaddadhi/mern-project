d215f732a1b412a2479d9961d74172248fbac24f
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-07-01 14:04:42 -0700

    fixed posting bug able to make posts now

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,24 +1,22 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getPosts } from '../../actions/post';
-import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
+import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? (
-    <Spinner />
-  ) : (
+  return (
     <Fragment>
-      <h1 className='large test-primary'>Posts</h1>
+      <h1 className='large text-primary'>Posts</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i> Welcome to the community
+        <i className='fas fa-user' /> Welcome to the community
       </p>
-      {/*Post Form */}
+      <PostForm />
       <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
@@ -29,7 +27,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
 };
 
 Posts.propTypes = {
-  action: PropTypes.func.isRequired,
+  getPosts: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
 };
 
